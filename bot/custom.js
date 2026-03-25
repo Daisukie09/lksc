2163e30d2d585eba63a9f69df5c55de224938a8f
Daisukie09 <165417091+Daisukie09@users.noreply.github.com>
2026-03-26 03:28:20 +0800

    Update custom.js

--- bot/custom.js
+++ bot/custom.js
@@ -1,6 +1,20 @@
 const { log } = global.utils;
 
 module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, getText }) {
+	// Register custom API extensions
+	if (typeof api.addExternalModule === 'function') {
+		try {
+			api.addExternalModule({
+				sendButtons: require('./extra/sendButtons')
+			});
+			log.info("CUSTOM API", "Successfully registered api.sendButtons");
+		} catch (err) {
+			log.error("CUSTOM API", "Failed to register api.sendButtons:", err);
+		}
+	} else {
+		log.warn("CUSTOM API", "api.addExternalModule is not available. Cannot register sendButtons.");
+	}
+
 	// This is where you can add your custom code to the bot.
 	// The bot will run this code every time it starts up (after logging in and loading data from the database).
 
